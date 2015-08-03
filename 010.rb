c3183654f2a8c2b71cb99e7d7f371134d3e63215
kentaro <takahashi.kentaro+github@gmail.com>
2015-08-04 01:06:27 +0900

    modified.

--- 010.rb
+++ 010.rb
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# only in ruby 1.9 
+# only in ruby 1.9
 
 # /* 10以下の素数の和は 2 + 3 + 5 + 7 = 17 である. */
 # /*     200万以下の全ての素数の和を求めよ. */
@@ -16,4 +16,3 @@ while small <= 2000000 do
 end
 
 puts sum
-
