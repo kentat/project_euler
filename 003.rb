d7f30f97d89c6dc0d22e2d04f38a8e29fac4696d
kentaro <takahashi.kentaro+github@gmail.com>
2015-08-04 01:05:32 +0900

    modified

--- 003.rb
+++ 003.rb
@@ -1,53 +1,19 @@
 # -*- coding: utf-8 -*-
-# only in ruby 1.9 
+# only in ruby 1.9
 
 # 13195 の素因数は 5, 7, 13, 29 である.
 # 600851475143 の素因数のうち最大のものを求めよ.
 
 require 'prime'
 
-# Prime.each(600851475143) do |prime|
-#   p prime #=> 2, 3, 5, 7, 11, ..., 97
-# end
-
 large = 600851475143
-small = 2
 
-while small < large do
-  small += 1
-  next unless small.prime?
-  if large % small == 0 then
-    large /= small 
+Prime.each(large)do |prime|
+  break if prime > large
+  while large % prime == 0 do
+      num = prime
+      large /= prime
   end
 end
 
-puts small
-
-    # long long int num   = 600851475143;
-
-# # for i in [600851475143..0].reverse
-# num = 600851475143
-# # i = 2
-# for i in(600851475143) do
-#   puts i
-# end
-
-
-
-# while i>0 do
-#   # puts i
-#   if i.prime? then
-#     puts i
-#     break
-#   end
-#   i = i - 1
-# end
-
-# 2.prime? #=> true
-# 4.prime? #=> false
-
-# if 7.prime? then
-#   puts "prime"
-# else
-#   puts "not prime"
-# end
+puts num
